7d6dcc6212f8ff16111b467df5121d4c147e0516
Thaize Paulo <thaizepaulo@gmail.com>
2023-06-14 11:41:59 -0300

    endpoint create para user.

--- src/main/java/br/com/dicasdeumdev/api/services/UserService.java
+++ src/main/java/br/com/dicasdeumdev/api/services/UserService.java
@@ -1,11 +1,12 @@
 package br.com.dicasdeumdev.api.services;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
     User findById(Integer id);
     List<User> findAll();
-
+    User create(UserDTO userDTO);
 }
