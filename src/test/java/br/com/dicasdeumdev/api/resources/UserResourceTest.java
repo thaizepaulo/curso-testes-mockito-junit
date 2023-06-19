db2b461851acc7d0d0cbba7581ca0692c40667da
Thaize Paulo <thaizepaulo@gmail.com>
2023-06-19 11:32:03 -0300

    Teste para endpoint FindById da classe UserResource implementado.

--- src/test/java/br/com/dicasdeumdev/api/resources/UserResourceTest.java
+++ src/test/java/br/com/dicasdeumdev/api/resources/UserResourceTest.java
@@ -10,6 +10,13 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserResourceTest {
@@ -32,7 +39,7 @@ class UserResourceTest {
     private UserService service;
 
     @Mock
-    private ModelMapper modelMapper;
+    private ModelMapper mapper;
 
     @BeforeEach
     void setUp() {
@@ -41,7 +48,21 @@ class UserResourceTest {
     }
 
     @Test
-    void findById() {
+    void whenFindByIdThenRetornSuccess() {
+        when(service.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(PASSWORD, response.getBody().getPassword());
     }
 
     @Test
